2c102309f36c056410789539bfcfbc74ff6ebe6b
Saketh <matligangireddy@gmail.com>
2022-09-06 01:48:33 +0000

    c2

--- Project1.java
+++ Project1.java
@@ -2,6 +2,8 @@ public class Project1
 {	
 	public static void main(string[] args){
 		System.out.println("Hello, Good Morning World!");
+		System.out.println("Hello, Good Morning World!");
+
 	}
 }
 
