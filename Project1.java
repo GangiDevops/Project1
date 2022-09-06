9ff54f2cc1291f8e2338e6a13221eeb1e2f36c67
GangiDevops <76618103+GangiDevops@users.noreply.github.com>
2022-09-06 07:20:05 +0530

    Update Project1.java
    C3

--- Project1.java
+++ Project1.java
@@ -2,7 +2,7 @@ public class Project1
 {	
 	public static void main(string[] args){
 		System.out.println("Hello, Good Morning World!");
-		System.out.println("Hello, Good Morning World!");
+		System.out.println("Hello, Good Evening World!");
 
 	}
 }
